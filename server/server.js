a1eee3da0695c129c4013929658d41fa8cd718e8
Shekhar saini <sainishekhar581@gmail.com>
2020-07-22 02:16:55 +0530

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -109,6 +109,19 @@ app.post('/users', (req, res) => {
     })
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 // POST /users/me
 app.get('/users/me', authenticate, (req, res) => {
     req.send(req.user);
