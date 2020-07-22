6f3e9c1b6ce83e9a16955ad4f8437e7539ff2e27
Shekhar saini <sainishekhar581@gmail.com>
2020-07-23 00:33:16 +0530

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,14 @@ app.post('/users/login', (req, res) => {
         res.status(400).send();
     });
 });
+// POST /user/me/token to logout
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.sendStatus(400).send();
+    });
+});
 
 // POST /users/me
 app.get('/users/me', authenticate, (req, res) => {
